6cf77bf09b9804e81cadae6683b1b26389893285
TechPersonYT <39715756+TechPersonYT@users.noreply.github.com>
2022-12-31 10:36:29 -0600

    Center camera height

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ impl Camera {
     fn render(&self, canvas: &mut Canvas, gfx: &GraphicsContext, visibles: Vec<&dyn Visible>) {
         let width = gfx.drawable_size().0;
         let height = gfx.drawable_size().1;
-        let position = vector![self.position.x + width / 2.0, self.position.y + height / 2.0];
+        let position = vector![self.position.x + width / 2.0, self.position.y - height / 2.0];
 
         for visible in visibles {
             visible.draw(canvas, gfx, &Point2{x: position.x, y: position.y}, self.rotation, height);
